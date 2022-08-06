39f92b21e8874680c9c3841dd815fb5da6ffd76b
lagleki <gleki.is.my.name@gmail.com>
2022-08-06 09:23:55 +0000

    Updates

--- assets/sance.js
+++ assets/sance.js
@@ -1,13 +1,23 @@
 function encodeValsiForWeb(v) {
-	return encodeURIComponent(v).replace(/'/g,"\\'").trim()
+  return encodeURIComponent(v).replace(/'/g, "\\'").trim();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-	var terms = Array.from(document.querySelectorAll(".guibutton"));
-	for (var i = 0; i < terms.length; i++) {
-		var Node = terms[i]
-		var slug = encodeValsiForWeb(Node.innerText);
-		var li = "<button class='tutci print:hidden' onclick=\"(function (){var s=new Audio('https://la-lojban.github.io/sutysisku/sance/lerfu/" + slug + ".ogg');s.play()})()\">▶</button>"
-		Node.parentNode.insertAdjacentHTML('beforeend', li)
-	}
+  renderButtons();
 });
+document.addEventListener("popstate", function () {
+  renderButtons();
+});
+
+function renderButtons() {
+  var terms = Array.from(document.querySelectorAll(".guibutton"));
+  for (var i = 0; i < terms.length; i++) {
+    var Node = terms[i];
+    var slug = encodeValsiForWeb(Node.innerText);
+    var li =
+      "<button class='tutci hide-for-print' onclick=\"(function (){var s=new Audio('https://la-lojban.github.io/sutysisku/sance/lerfu/" +
+      slug +
+      ".ogg');s.play()})()\">▶</button>";
+    Node.parentNode.insertAdjacentHTML("beforeend", li);
+  }
+}
